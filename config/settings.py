f5c8d09b663e450003ffa52224bf20ac8458618b
Oneyura <hryn.yuri@gmail.com>
2025-05-18 21:17:09 +0300

    create tests for planetarium

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,7 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-   "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-   "PAGE_SIZE": 10
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+    "TEST_REQUEST_DEFAULT_FORMAT": "json"
 }
