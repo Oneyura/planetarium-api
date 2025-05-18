4e01e81b12fbaa7cea7a15e0dc4cff9a95c30efb
Oneyura <hryn.yuri@gmail.com>
2025-05-18 20:14:18 +0300

    Create planetarium/admin.py

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Ticket, Reservation)
+
+admin.site.site_header = "Planetarium Administration"
+admin.site.site_title = "Planetarium Administration"
+admin.site.index_title = "Welcome to Planetarium Administration"
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
+admin.site.register(Reservation)
